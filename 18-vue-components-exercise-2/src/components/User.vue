107707f2a20a53707af012469d872ea2ed42963a
Tobias E <gilian@web.de>
2020-01-18 00:10:00 +0100

    Add the final part of chapter 8 - components

---
+++ 18-vue-components-exercise-2/src/components/User.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="component">
+        <h1>The User Component</h1>
+        <p>I'm an awesome User!</p>
+        <button @click="changeName">Change Name</button>
+        <button @click="resetName">Reset</button>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <app-user-detail :myName="myName" @nameWasReset="myName = $event"></app-user-detail>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <app-user-edit></app-user-edit>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import UserDetail from './UserDetail.vue';
+    import UserEdit from './UserEdit.vue';
+
+    export default {
+        data: function() {
+            return {
+                myName: 'Tobias'
+            };
+        },
+        methods: {
+            changeName() {
+                this.myName = 'Anna';
+            },
+            resetName() {
+                this.myName = 'Tobi'
+            }
+        } ,
+        components: {
+            'app-user-detail': UserDetail,
+            appUserEdit: UserEdit
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightblue;
+    }
+</style>
---
+++ 18-vue-components-exercise-2/src/components/UserDetail.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>This is the name: {{ switchName() }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            myName: [String, Array]
+        },
+        methods: {
+            switchName() {
+                return this.myName.split("").reverse().join("");
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
